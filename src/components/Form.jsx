5d1fe9f8964ebfbaad814eac1999accf0f8eb071
Pooja Sagar Ambi <pooja.a@thinkbridge.in>
2024-02-09 18:17:16 +0530

    icon cahnge

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,7 @@ const Form = () => {
             <div className="row">
                 <div className="col-md-3"></div>
                 <div className="col-md-6 ">
-                    <h5 className="text-start mt-2 pb-3 title font-16 fw-bold">Appoinment Booking</h5>
+                    <h5 className="text-start mt-2 title font-16 fw-bold">Appoinment Booking</h5>
                     <form className='form-block p-4 rounded-5 mt-2'>
                         <div className="mb-3">
                             <input type="text" className="form-control rounded-5 " placeholder="Enter Name"/>
@@ -25,7 +25,7 @@ const Form = () => {
                             <input type="text" className="form-control rounded-5 " placeholder="Choose Consultation"/>
                         </div>
                         <div className="row m-1">
-                        <button type="submit" className="btn btn-lg bg-button rounded-5 font-16">Book Appoinment</button>
+                        <button type="submit" className="btn btn-lg bg-button rounded-5 font-16 fw-semibold ">Book Appoinment</button>
                         </div>
                        
                     </form>
